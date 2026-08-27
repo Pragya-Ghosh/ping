0ea824ae116fb2bad2770c0398daba7c54fad4a0
Pragya Ghosh <pragyarashmighosh@gmail.com>
2026-08-27 20:57:36 +0530

    proto: client can connect to google server, send an http request and receive a response from the same

--- ClientSocket/main.c
+++ ClientSocket/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
 
 int main() {
     
@@ -11,14 +13,36 @@ int main() {
     /*connect to server*/
     //configure server address
     struct sockaddr_in saddr = {
-        .sin_port = htons(7000),
+        .sin_port = htons(80),
         .sin_family = AF_INET, 
-        .sin_addr.s_addr = inet_addr("235.3.42.1")
+        .sin_addr.s_addr = inet_addr("172.217.26.46")
     };
 
     //connect to server
-    int connectFD = connect(socketFD, &saddr, sizeof(saddr));
+    int connectFD = connect(socketFD, (struct sockaddr *)&saddr, sizeof(saddr));
     
+    if (connectFD == 0)
+        printf("Connection was successful\n");
+    else
+     printf("No\n");
+
+
+    /*send some data to the server*/
+    char* message;
+    //http request from client
+    message = "GET \\ HTTP/1.1\r\nHost:google.com\r\n\r\n";
+    send(socketFD, message, strlen(message), 0);
+
+    /*receive a response from server*/
+    char buffer[1024];
+    //returns the number of bytes received
+    int n = recv(socketFD, buffer, 1023, 0);
+    if(n > 0) {
+        buffer[n] = '\0'; 
+        printf("Response was: %s\n", buffer);
+    }
+
+    close(socketFD);
 
     return 0;
 }
